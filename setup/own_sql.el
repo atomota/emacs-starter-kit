75c328f642d58e2e264b4fb421d5dbc45f3c7f56
Achim Tromm <atromm@timocom.com>
2016-06-28 15:05:35 +0200

    consider markdown-mode and sql-mode

---
+++ setup/own_sql.el
@@ -0,0 +1,5 @@
+(add-to-list 'auto-mode-alist '("\\.sql$" . sql-mode))
+(add-to-list 'auto-mode-alist '("\\.pdc$" . sql-mode))
+(add-to-list 'auto-mode-alist '("\\.pck$" . sql-mode))
+(add-to-list 'auto-mode-alist '("\\.fnc$" . sql-mode))
+(provide 'own_sql)
--- setup/own_text.el
+++ setup/own_text.el
@@ -5,4 +5,6 @@
     ;(add-hook 'text-mode-hook 'turn-on-flyspell)
 ))
 
+(add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
+
 (provide 'own_text)
