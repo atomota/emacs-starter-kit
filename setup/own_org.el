04f0d457dbd26dd061ee51eb56bbbc767b08f962
A.T <github@tromm.net>
2016-06-28 22:43:37 +0200

    split up common and local config

--- setup/own_org.el
+++ setup/own_org.el
@@ -1,4 +1,9 @@
 ;(require 'org-install)
+
+(setq org-todo-keywords '((sequence "TODO(t)" "NEXT(n)" "SOMEDAY(y)" "STARTED(s)" "WAITING(w)" "DELEGATED(l)" "|" "DONE(d)" "CANCELLED(c)")))
+
+(setq org-tag-alist '(("PHONE" . ?p) ("STUDY" . ?y) ("READ" . ?r) ("SHOP" . ?s) ("WORK" . ?w)))
+
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
@@ -11,29 +16,25 @@
     (flyspell-mode 0) 
 ))
 
-(setq org-directory "~/Documents/org")
-(setq org-agenda-files (quote ("~/Documents/org/")))
-(setq org-mobile-directory "~/Documents/org/stage")
-(setq org-mobile-inbox-for-pull "~/Documents/org/from-mobile.org")
-
 ;; add hooks for own webdav/ssh sync
-(add-hook 'org-mobile-post-push-hook
-  (lambda () (shell-command "my_org_mobile_push")))
-(add-hook 'org-mobile-pre-pull-hook
-  (lambda () (shell-command "my_org_mobile_pre_pull")))
-(add-hook 'org-mobile-post-pull-hook
-  (lambda () (shell-command "my_org_mobile_post_pull")))
-
-
-;; following allows mobile org to sync automatically via cron
-(defvar my-org-mobile-sync-timer nil)
-(defvar my-org-mobile-sync-secs (* 60 15))
+;; (add-hook 'org-mobile-post-push-hook
+;;   (lambda () (shell-command "my_org_mobile_push")))
+;; (add-hook 'org-mobile-pre-pull-hook
+;;   (lambda () (shell-command "my_org_mobile_pre_pull")))
+;; (add-hook 'org-mobile-post-pull-hook
+;;   (lambda () (shell-command "my_org_mobile_post_pull")))
 
 (defun my-org-mobile-sync-pull-and-push ()
   "Start pulling via `org-mobile-pull' and afterwards pushing `org-mobile-push' of org mode data"
   (interactive)
   (org-mobile-pull)
   (org-mobile-push))
+
+;; following allows mobile org to sync automatically via cron
+;(my-org-mobile-sync-start)
+
+(defvar my-org-mobile-sync-timer nil)
+(defvar my-org-mobile-sync-secs (* 60 15))
 
 (defun my-org-mobile-sync-start ()
   "Start automated `org-mobile-push'"
@@ -46,8 +47,5 @@
   "Stop automated `org-mobile-push'"
   (interactive)
   (cancel-timer my-org-mobile-sync-timer))
-
-;(my-org-mobile-sync-start)
-
 
 (provide 'own_org)
