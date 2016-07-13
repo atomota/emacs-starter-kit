38bea2d97ffd194187d1445fcd6ab93adfbdfcc4
A.T <github@tromm.net>
2016-07-13 23:14:36 +0200

    set color theme for gnu/linux

--- setup/own_color_theme.el
+++ setup/own_color_theme.el
@@ -1,11 +1,10 @@
 (progn
-
   (require 'color-theme)
   (if (or (equal window-system 'x)
           (equal window-system 'mac))
       (progn
-        (if (or (eq system-type 'windows-nt)
-                (eq system-type 'cygwin))
+        (if (or (equal system-type 'windows-nt)
+                (equal system-type 'cygwin)
+                (equal system-type 'gnu/linux))
             (color-theme-zenburn) (color-theme-blackboard)))))
-
 (provide 'own_color_theme)
