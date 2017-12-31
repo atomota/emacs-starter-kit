2b322f28d13e12a84a8532ac73e35c1fa27f9ae5
Achim Tromm <github@tromm.net>
2017-12-31 17:04:51 +0100

    Suppress warnings which are false positives

--- setup/000_own_package.el
+++ setup/000_own_package.el
@@ -1,8 +1,26 @@
 (progn
   (require 'package)
 
+  ;; Workaround to get rid of (false positives) warnings, see:
+  ;;
+  ;; https://github.com/flyingmachine/emacs-for-clojure/issues/26
+  ;; https://github.com/DarwinAwardWinner/ido-completing-read-plus/issues/35
+
+  (defvar ido-cur-item nil)
+  (defvar ido-default-item nil)
+  (defvar ido-cur-list nil)
+  (defvar predicate nil)
+  (defvar inherit-input-method nil)
+  (defvar multiple-cursors-mode nil)
+  (defvar mc--read-char nil)
+  (defvar mc--read-char nil)
+  (defvar multiple-cursors-mode nil)
+  (defvar mc--read-quoted-char nil)
+  (defvar mc--read-quoted-char nil)
+  (defvar rectangular-region-mode nil)
+  
   ;; add gnu packages here , see https://elpa.gnu.org/
-  (defvar my-packages '(muse js2-mode))
+  (defvar my-packages '(js2-mode))
 
   ;; further package source marmalade
   (add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
