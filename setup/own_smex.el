f29b404f4b68d8fb1890cbde972f970394d3ed88
Achim Tromm <github@tromm.net>
2018-09-22 22:01:30 +0200

    Deactivate smex

--- setup/own_smex.el
+++ setup/own_smex.el
@@ -1,9 +1,9 @@
 ; Not needed if you use package.el
-(require 'smex) 
+;(require 'smex) 
 
 ; Can be omitted. This might cause a (minimal) delay
 ; when Smex is auto-initialized on its first run.
-(smex-initialize) 
+;(smex-initialize) 
 
 (global-set-key (kbd "M-x") 'smex)
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
