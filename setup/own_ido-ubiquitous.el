b7b25a7e3eae19e50a0bc387f0fe5491d6b176cf
Achim Tromm <github@tromm.net>
2018-10-11 20:16:01 +0200

    Isolate config of obsolete wn_ido-ubiquitous

--- setup/own_ido-ubiquitous.el
+++
@@ -1,8 +0,0 @@
-;; org-mode and magit have their own support for ido
-(setq org-completion-use-ido t)
-(setq magit-completing-read-function 'magit-ido-completing-read)
-
-(require 'ido-ubiquitous)
-(ido-ubiquitous-mode 1)
-
-(provide 'own_ido-ubiquitious)
