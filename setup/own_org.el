6898eae69af7fee9948ff88a615a6b63709ad4d8
Achim Tromm <github@tromm.net>
2018-10-23 18:33:24 +0200

    Remove setup for mobile org

--- setup/own_org.el
+++ setup/own_org.el
@@ -1,9 +1,8 @@
-;; setup local/mobile environment via client-specific config
-;
+;; setup local/mobile environment by client-specific config
+
 ;(setq org-directory "~/org")
 ;(setq org-agenda-files (quote ("~/org/")))
-;(setq org-mobile-directory "~/mobile-org/")
-;(setq org-mobile-inbox-for-pull "/var/tmp/from-mobile.org")
+
 
 (setq org-todo-keywords '((sequence "TODO(t)" "NEXT(n)" "SOMEDAY(y)" "STARTED(s)" "WAITING(w)" "DELEGATED(l)" "|" "DONE(d)" "CANCELLED(c)")))
 
@@ -20,37 +19,5 @@
     ;(force-flyspell-mode)
     (flyspell-mode 0) 
 ))
-
-;; add hooks for own webdav/ssh sync
-;; (add-hook 'org-mobile-post-push-hook
-;;   (lambda () (shell-command "my_org_mobile_push")))
-;; (add-hook 'org-mobile-pre-pull-hook
-;;   (lambda () (shell-command "my_org_mobile_pre_pull")))
-;; (add-hook 'org-mobile-post-pull-hook
-;;   (lambda () (shell-command "my_org_mobile_post_pull")))
-
-(defun my-org-mobile-sync-pull-and-push ()
-  "Start pulling via `org-mobile-pull' and afterwards pushing `org-mobile-push' of org mode data"
-  (interactive)
-  (org-mobile-pull)
-  (org-mobile-push))
-
-;; following allows mobile org to sync automatically via cron
-;(my-org-mobile-sync-start)
-
-(defvar my-org-mobile-sync-timer nil)
-(defvar my-org-mobile-sync-secs (* 60 15))
-
-(defun my-org-mobile-sync-start ()
-  "Start automated `org-mobile-push'"
-  (interactive)
-  (setq my-org-mobile-sync-timer
-        (run-with-idle-timer my-org-mobile-sync-secs t
-                             'my-org-mobile-sync-pull-and-push)))
-
-(defun my-org-mobile-sync-stop ()
-  "Stop automated `org-mobile-push'"
-  (interactive)
-  (cancel-timer my-org-mobile-sync-timer))
 
 (provide 'own_org)
